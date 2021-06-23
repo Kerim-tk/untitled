f19075546eed3ae93882425f44fbe6edf9bf7b2b
Kerim-tk <alekperovkerim@gmail.com>
2021-06-23 11:29:20 +0400

    Welcome to Java!

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.Scanner;
+
+import javax.sound.midi.SysexMessage;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
